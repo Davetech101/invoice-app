197c803e7362083f6f08c95b443c481c87018584
Ogaranya <enomahog@gmail.com>
2023-05-31 04:32:16 +0100

    structure

---
+++ components/Header.jsx
@@ -0,0 +1,14 @@
+import SHeader from "@/styles/styled-components/Header";
+import React from "react";
+
+const Header = () => {
+  return (
+    <SHeader>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere ea tempore
+      ullam quibusdam error, soluta corporis, nisi, debitis autem perspiciatis
+      quos vitae. Nisi labore tempore beatae voluptatem autem deserunt maiores?
+    </SHeader>
+  );
+};
+
+export default Header;
---
+++ styles/styled-components/Header.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const SHeader = styled.header`
+  background: red;
+`;
+
+export default SHeader;
