50a53414db0490ecfe9d7f3beaf3a258eb749545
Ogaranya <enomahog@gmail.com>
2023-05-31 12:40:06 +0100

    invoice form

--- components/InvoiceForm.jsx
+++ components/InvoiceForm.jsx
@@ -50,24 +50,52 @@ const InvoiceForm = () => {
             <div className="email sec">
               <label htmlFor="email">Client's Email</label>
 
-              <input type="text" id="email" />
+              <input type="email" id="email" />
+            </div>
+
+            <div className="clStreet sec">
+              <label htmlFor="clStreet">Street Address</label>
+
+              <input type="text" id="clStreet" />
             </div>
 
             <div className="zone">
-              <div className="city sec">
-                <label htmlFor="city">City</label>
-                <input type="text" id="city" />
+              <div className="clCity sec">
+                <label htmlFor="clCity">City</label>
+                <input type="text" id="clCity" />
               </div>
 
-              <div className="code sec">
-                <label htmlFor="code">Post Code</label>
-                <input type="text" id="code" />
+              <div className="clCode sec">
+                <label htmlFor="clCode">Post Code</label>
+                <input type="text" id="clCode" />
               </div>
             </div>
 
-            <div className="country sec">
-              <label htmlFor="country">Country</label>
-              <input type="text" id="country" />
+            <div className="clCountry sec">
+              <label htmlFor="clCountry">Country</label>
+              <input type="text" id="clCountry" />
+            </div>
+          </div>
+
+          <div className="dates">
+            <div className="invDate">
+              <label htmlFor="invoiceDate">Invoice Date</label>
+
+              <input type="date" id="invoiceDate" />
+            </div>
+
+            <div className="terms">
+              <label htmlFor="terms">Payment Terms
+              <p>(IN DAYS)</p>
+              </label>
+
+              <input type="number" max="30" min="1" id="terms" />
+            </div>
+
+            <div className="proDesc">
+              <label htmlFor="description">Project Description</label>
+
+              <input type="text" id="description" />
             </div>
           </div>
         </form>
